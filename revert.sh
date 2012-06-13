cb159100f2b1b55b26b403bf107ca4b7c2baa837
Jeremy Archer <open-source@fatlotus.com>
2012-06-13 14:10:13 -0500

    Reorganize file structure.

--- revert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# copy over original input.csv
-# cp /var/www/chicagowebdice/data/
-
-
-# copy over original output.csv
-cp /var/www/chicagowebdice/data/output.csv /var/www/chicagowebdice/output.csv
