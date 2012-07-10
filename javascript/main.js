0abbc14bdef51fc5eb42f5a89d8967741618c19b
Jeremy Archer <open-source@fatlotus.com>
2012-07-10 15:06:34 -0500

    Fix interface problems when displaying the overlay.

--- javascript/main.js
+++ javascript/main.js
@@ -231,12 +231,16 @@
 			if (visible) {
 				var overlayVisibility = 1.0;
 				var advancedHelpOffset = 0;
+				var overlayDisplay = 'block';
 			} else {
 				var overlayVisibility = 0.0;
 				var advancedHelpOffset = 200;
+				var overlayDisplay = 'none';
 			}
 			
-			$(overlayDiv).animate({ opacity : overlayVisibility });
+			$(overlayDiv).css({ display : 'block' }).animate({ opacity : overlayVisibility }, function() {
+				$(overlayDiv).css({ display : overlayDisplay });
+			});
 			$(advancedHelpDiv).animate({ top : advancedHelpOffset });
 		}
 		
