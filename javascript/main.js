e4bf16a3c1692e6efadca5c778bb09be4b1d44c2
Aidan Sadowski <amsadowski@cps.edu>
2012-08-02 09:34:25 -0500

    Added autoscrolldown

--- javascript/main.js
+++ javascript/main.js
@@ -459,7 +459,7 @@
 			$(runsUL).animate({ height:outerHeight, scrollTop : (bottomHeight - outerHeight) }, "slow");
 			$(sidebarDiv).animate( { height: '100%' }, "slow");
 			$(contentDiv).animate( { height: '100%' }, "fast");
-			$(contentDiv).animate( { scrollTop: 155 }, "slow");
+			window.scrollBy(0,155);
 		}
 		
 		var setVisibilityOfOverlay = function(visible) {
