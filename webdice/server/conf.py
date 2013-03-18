e933139b16733bf3b2ae118a9025d957237e0428
Nathan Matteson <matteson@obstructures.org>
2013-03-18 13:34:08 -0500

    Begin fixing YAML file, and move to flask

--- webdice/server/conf.py
+++ webdice/server/conf.py
@@ -1,340 +1,112 @@
 import yaml
 import HTMLParser
 import re
+import pprint
 
+
+pp = pprint.PrettyPrinter(indent=4)
 htmlp = HTMLParser.HTMLParser()
-YAML = file('../parameters.yaml', 'r')
+YAML = file('../parameters2.yaml', 'r')
 CONF_FILE = yaml.load(YAML)
 YAML.close()
-basic_help = CONF_FILE['basic_help']
-intermediate_help = CONF_FILE['intermediate_help']
-advanced_help = CONF_FILE['advanced_help']
 
 def format_for_web(text):
-    return re.sub(r'/^\(([^)]+)\)/', '<sup>$1</sup>',
-        re.sub(r'/_\(([^)]+)\)/', '<sub>$1</sub>',
-            htmlp.unescape(text)))
+    return re.sub(
+        r'/^\(([^)]+)\)/', '<sup>$1</sup>',
+        re.sub(
+            r'/_\(([^)]+)\)/', '<sub>$1</sub>',
+            htmlp.unescape(text)
+        )
+    )
 
-def is_num(n):
-    try: float(n)
-    except (ValueError, TypeError), e: print e
-
-def options(p, opts, opt):
-    try: p[opt]
-    except KeyError: pass
-    else: opts.append(opt)
-
-def get_all_parameters():
-    tabs, parameters, all_parameters, selected = build_data()
-    return all_parameters
-
-def get_measurements():
-    measurements, graphs, graph_locations = build_measurements()
-    return measurements
-
-def get_graph_locations():
-    measurements, graphs, graph_locations = build_measurements()
-    return graph_locations
-
-def build_data():
-    """
-    Build objects from YAML file.
-    ...
-    Args: None
-    Returns:
-        [tabs, parameters, all_parameters, selected_tab]
-    """
-    parameters = CONF_FILE['parameters']
-    tabs = {}
-    all_parameters = {}
-    selected_tab = 'basic'
-    for parameter in parameters:
-        is_select_control = False
-        is_range_control = False
-        is_submit_control = False
-        required = ["name", "machine_name", "section", "tab"]
-        optional = []
-        try: parameter['min']
-        except KeyError: parameter['is_range_control'] = False
-        else:
-            required.extend(["min", "max", "default", "precision"])
-            is_range_control = True
-            parameter['is_range_control'] = True
-        try: parameter['values']
-        except KeyError: parameter['is_select_control'] = False
-        else:
-            required.append('values')
-            is_select_control = True
-            parameter['is_select_control'] = True
-        try: parameter['id']
-        except KeyError: parameter['is_submit_control'] = False
-        else:
-            required.extend(['id', 'button_name'])
-            is_submit_control = True
-            parameter['is_submit_control'] = True
-        options(parameter, optional, 'step')
-        options(parameter, optional, 'description')
-        options(parameter, optional, 'unit')
-        options(parameter, optional, 'subheading')
-        options(parameter, optional, 'disabled')
-        size = len(required) + len(optional)
-        #        if len(parameter) > size:
-        #PHP:            trigger_error(sprintf(too_many_items, count(parameter) - size), E_USER_ERROR);
-        #        for name in required:
-        #PHP:            if (!isset(parameter[name]))
-        #PHP:            trigger_error(sprintf(missing_parameter, name), E_USER_ERROR);
-        name = parameter['name']
-        machine_name = parameter['machine_name']
-        tab_name = parameter['tab']
-        cleaned_tab_name = re.sub(r'[^a-z]', '', str(parameter['tab']).lower())
-        section_name = parameter['section']
-        cleaned_section_name = re.sub(r'[^a-z]', '', str(parameter['section']).lower())
-        if is_select_control:
-            parameter['indexed_values'] = {}
-            values = parameter['values']
-            for value in values:
-                try: value['name']
-                except KeyError: pass #trigger_error(option_no_name, E_USER_ERROR);
-                try: value['machine_name']
-                except KeyError: pass #trigger_error(option_no_machine_name, E_USER_ERROR);
-                option_machine_name = value['machine_name']
-                try: parameter['indexed_values'][option_machine_name]
-                except KeyError: pass #trigger_error(duplicate_option, E_USER_ERROR);
-                parameter['indexed_values'][option_machine_name] = value
-        elif is_range_control:
-            pass
-        #            if (!is_numeric(parameter['min']) ||
-        #            !is_numeric(parameter['max']) ||
-        #            !is_numeric(parameter['step']) ||
-        #            !is_numeric(parameter['precision']) ||
-        #            !is_numeric(parameter['default']))
-        #            trigger_error(non_numeric_configuration, E_USER_ERROR);
-        #
-        #            if ((parameter['default'] > parameter['max']) ||
-        #            (parameter['default'] < parameter['min']))
-        #            trigger_error(invalid_default, E_USER_ERROR);
-        #            }
-        try: all_parameters[machine_name]
-        except KeyError, e:
-            all_parameters[machine_name] = parameter
-        #        else: print e
-        try: tabs[cleaned_tab_name]
-        except KeyError:
-            tabs[cleaned_tab_name] = {
-                'name': tab_name,
-                'sections': {},
-                'html_id': 'tab-'+cleaned_tab_name,
-                }
-            if selected_tab is None:
-            #                selected_tab = &tabs[cleaned_tab_name]
-                selected_tab = tabs[cleaned_tab_name]
-
-        try: tabs[cleaned_tab_name]['sections'][cleaned_section_name]
+def get_defaults(section):
+    defaults = {}
+    for var in ['min', 'max', 'step', 'default', 'precision']:
+        try:
+            defaults[var] = section[var]
         except:
-            tabs[cleaned_tab_name]['sections'][cleaned_section_name] = {
-                'name': section_name,
-                'parameters': [],
-                }
-        tabs[cleaned_tab_name]['sections'][cleaned_section_name]['parameters'].append(parameter)
-    return tabs, parameters, all_parameters, selected_tab
+            defaults[var] = False
+    return defaults
 
-def build_measurements():
-    """
-    Build objects from YAML file.
-    ...
-    Args: None
-    Returns:
-        [measurements, graphs, graph_locations]
-    """
-    measurements = CONF_FILE['measurements']
-    graphs = {}
-    graph_locations = ['topleft', 'topright', 'bottomleft', 'bottomright']
-    for measurement in measurements:
-        required = ['name', 'machine_name', 'unit']
-        optional = []
-        for name in required:
-            try: measurement[name]
-            except KeyError, e: pass
-        options(measurement, optional, 'description')
-        options(measurement, optional, 'format')
-        options(measurement, optional, 'location')
-        size = len(required) + len(optional)
-        #        if (count($measurement) > $size)
-        #        trigger_error(sprintf($too_many_items, count($measurement) - $size), E_USER_ERROR);
-        try: measurement['location']
-        except KeyError, e: pass
-        else:
-            location = measurement['location']
-            try: graphs[location]
+def set_from_defaults(parameter, defaults):
+    for key, val in defaults.iteritems():
+        if val is not False:
+            try:
+                parameter[key]
             except KeyError:
-                graphs[location] = measurement
-            #            else: trigger_error(sprintf($duplicate_graph, $location), E_USER_ERROR);
-    for location in graph_locations:
-        try: graphs[location]
-        except KeyError, e: pass
-    return measurements, graphs, graph_locations
+                parameter[key] = val
+    print parameter
+    return parameter
 
-def build_questions():
-    questions = CONF_FILE['questions']
-    for question in questions:
-        required = ['question', 'answer']
-        for name in required:
-            try: question[name]
-            except: pass
-            else: pass #trigger_error(sprintf(missing_parameter, name), E_USER_ERROR);
-        size = len(required)
-        if len(question) > size:
-            pass #trigger_error(sprintf(too_many_items, count(measurement) - size), E_USER_ERROR);
-
-def tabs_html():
-    """
-    Build HTML for inputs on left side of webpage.
-    ...
-    Args: None
-    Returns:
-        HTML
-    """
-    tabs, parameters, all_parameters, selected_tab = build_data()
-    html = ''
-    if len(tabs) <= 1:
-        html += '<div id="parameters">\n'
+def set_input_type(parameter, key, type):
+    try:
+        parameter[key]
+    except KeyError:
+        parameter[type] = False
     else:
-        html += '<div id="parameters" class="has-tabs">\n'
-    html += '<div id="sidebar-tabs" class="tabs">\n'
-    for tab in tabs:
-        tab_name = format_for_web(tabs[tab]['name'])
-        tab_id = htmlp.unescape(tabs[tab]['html_id'])
-        if tab == selected_tab:
-            html += '<a href="" class="selected" id="link-to-'+tab_id+'">'+tab_name+'</a>\n'
-        else:
-            html += '<a href="" id="link-to-'+tab_id+'">'+tab_name+'</a>\n'
-    html += '</div>\n'
-    for tab in tabs:
-        tab_id = htmlp.unescape(tabs[tab]['html_id'])
-        sections = tabs[tab]['sections']
-        if tab == selected_tab:
-            html += '<div id="'+tab_id+'" class="tab selected">\n'
-        else:
-            html += '<div id="'+tab_id+'" class="tab notselected">\n'
-        for section in sections:
-            section_name = format_for_web(sections[section]['name'])
-            parameters = sections[section]['parameters']
-            html += '<h2>'+section_name+'</h2>\n'
-            html += '<ul>\n'
+        parameter[type] = True
+
+def set_tick(parameter):
+    try:
+        parameter['unit']
+    except KeyError:
+        parameter['unit'] = ''
+    minimum = parameter['min']
+    maximum = parameter['max']
+    default = parameter['default']
+    parameter['tick'] = '%i%%' % (
+        (((float(default) - minimum)/(maximum - minimum)) * 100.) - 50.
+    )
+
+def get_parameters(section):
+    try:
+        parameters = section['parameters']
+    except KeyError:
+        parameters = []
+    return parameters
+
+def get_sections(tab):
+    try:
+        sections = tab['sections']
+    except:
+        sections = []
+    return sections
+
+def get_radio(section):
+    try:
+        radio = section['radio']
+        return True
+    except:
+        return False
+
+def get_tabs():
+    tabs = CONF_FILE['tabs']
+    for tab in [tabs[0]]:
+        print tab
+        policy = False
+        if tab['name'] == 'Policy':
+            policy = True
+        for section in get_sections(tab):
+            print section['name']
+            defaults = get_defaults(section)
+            if policy and get_radio(section):
+                button = '<input type="radio" value="treaty" name="policy_type" style="width:auto"'
+                if section['machine_name'] == 'default':
+                    button += ' checked="checked"'
+                button += '/>'
+                section['name'] = button + section['name']
+            parameters = get_parameters(section)
             for parameter in parameters:
-            #                    name = format_for_web(parameter['name'])
-                name = parameter['name']
-                machine_name = htmlp.unescape(parameter['machine_name'])
-                is_select_control = parameter['is_select_control']
-                is_range_control = parameter['is_range_control']
-                is_submit_control = parameter['is_submit_control']
-                try: parameter['disabled']
-                except:
-                    disabled = ''
-                    clas = ''
-                else:
-                    disabled = 'disabled="disabled"'
-                    clas = 'class="disabled"'
-                try: parameter['description']
-                except:
-                    try: parameter['unit']
-                    except: html += '<li><label %s>%s ' % (clas, name)
-                    else:
-                        unit = parameter['unit']
-                        html += '<li %s>%s (%s) ' % (clas, name, unit)
-                else:
-                    description = htmlp.unescape(parameter['description'])
-                    html += '<li><label title="%s" %s>%s ' % (description, clas, name)
-                if is_select_control:
-                    html += '</label>\n'
-                    values = parameter['values']
-                    html += '<select name="%s" %s>\n' % (machine_name, disabled)
-                    for value in values:
-                        option_machine_name = value['machine_name']
-                        option_name = value['name'];
-                        try: value['description']
-                        except: html += '<option id="%s">%s</option>\n' % (option_machine_name, option_name)
-                        else:
-                            description = htmlp.unescape(value['description'])
-                            html += '<option id="%s" ' % option_machine_name
-                            html += 'title="%s">%s</option>\n' % (description, option_name)
-                    html += '</select></li>\n'
-                elif is_range_control:
-                    min = parameter['min']
-                    max = parameter['max']
-                    step = parameter['step']
-                    default = parameter['default']
-                    precision = parameter['precision']
-                    tickMarkLeft = ((float(default) - min)/(max - min)) * 100.
-                    tickMarkLeft = tickMarkLeft - 50
-                    tickMarkLeftWithUnit = '%i%%' % tickMarkLeft
-                    try: parameter['unit']
-                    except:
-                        html += '<span class="label"><span class="label-number">%s</span></span></label> <input name="%s" %s ' % (
-                            default, machine_name, disabled)
-                    else:
-                        unit = parameter['unit']
-                        html += '<span class="label"><span class="label-number">%s</span><span>%s</span></span></label> <input name="%s" %s ' % (
-                            default, unit, machine_name, disabled)
-                    html += 'type="range" min="%s" max="%s" step="%s" value="%s" data-prec="%s"/>\n' % (
-                        min, max, step, default, precision)
-                    html += '<div class="tick-wrap"><span class="tick" style="left:%s" %s>&bullet;</span></div></li>\n' % (
-                        tickMarkLeftWithUnit, clas)
-                elif is_submit_control:
-                    id = parameter['id']
-                    button_name = parameter['button_name']
-                    html += '</label><input type="submit" id="%s" value="%s"/></li>' % (id, button_name)
-            html += '</ul>\n'
-        html += '</div>'
-    return html
+                parameter = set_from_defaults(parameter, defaults)
+                parameter = set_input_type(parameter, 'min', 'range')
+                parameter = set_input_type(parameter, 'values', 'select')
+                parameter = set_tick(parameter)
 
-def paragraphs_html(t):
-    """
-    Build HTML for initial text on webpage.
-    ...
-    Args: None
-    Returns:
-        HTML
-    """
-    initial_help = CONF_FILE['initial_help'][t]
-    html = ''
-    paragraphs = initial_help.split('\n')
-    for paragraph in paragraphs:
-        html += '<p>%s</p>\n' % paragraph
-    return html
 
-def measurements_html():
-    """
-    Build HTML for options in select tabs, and columns in CSV.
-    ...
-    Args: None
-    Returns:
-        HTML
-    """
-    measurements = get_measurements()
-    html = ''
-    for section in measurements:
-        html += '<optgroup label="-- %s --"></optgroup>\n' % htmlp.unescape(section['name'])
-        for measurement in section['values']:
-            machine_name = htmlp.unescape(measurement['machine_name'])
-            measurement_name = htmlp.unescape(measurement['name'])
-            html += '<option value="%s">%s</option>\n' % (machine_name, measurement_name)
-    return html
 
-def foo():
-    pass
-    #    #The next 100 lines or so are just checking to make sure all the data for each input is set
-    #    missing_parameter = "Missing \"%s\" attribute on configuration element."
-    #    too_many_items = "Configuration_element has %d extra element(s)."
-    #    option_no_name = "Parameter option element has no \"name\" option and should."
-    #    option_no_machine_name = "Parameter option element has no \"machine_name\" option and should."
-    #    non_numeric_configuration = "The \"min,\" \"max,\" and \"step\" options are numeric."
-    #    duplicate_property = "Multiple parameters with machine name \"%s\"."
-    #    duplicate_option = "Multiple options with machine_name \"%s\"."
-    #    invalid_default = "The default must be between the minimum and maximum."
-    #    no_graph_in_location = "There is no graph set to display in location %s."
-    #    tabs = {}
-    #    all_parameters = {}
-    #    selected_tab = None
-    #    graphs = {}
-    #    graph_locations = ['topleft', 'topright', 'bottomleft', 'bottomright']
+                # pp.pprint( tabs[0])
+                # for subsection in get_sections(section):
+
+                # for numinput in section['parameters']:
+                #     numinput['disabled'] = True
+                #     print numinput
