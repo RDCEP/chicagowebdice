5b612e9ef9be1323ca09bdb0b101b9912d9dd483
Nathan Matteson <matteson@obstructures.org>
2013-06-14 17:58:49 -0500

    Move set_model() into Loop()

--- webdice/equations/loop.py
+++ webdice/equations/loop.py
@@ -1,14 +1,24 @@
-from damages import DiceDamages
-from carbon import DiceCarbon
+import carbon, damages
 from emissions import DiceEmissions
 from consumption import DiceConsumption
 from utility import DiceUtility
 from productivity import DiceProductivity
 
 class Loop(object):
-    """Provide getters and setters for model equations"""
-    def __init__(self, params, damages_model=DiceDamages,
-                 carbon_model=DiceCarbon):
+    """
+    Provide getters and setters for model equations
+    ...
+    Args
+    ----
+    params : obj
+    ...
+    Kwargs
+    ------
+    damages_model : obj
+    carbon_model : obj
+    """
+    def __init__(self, params, damages_model=damages.DiceDamages,
+                 carbon_model=carbon.DiceCarbon):
         self._damages_model = damages_model
         self._carbon_model = carbon_model
         self._emissions_model = DiceEmissions(params)
@@ -63,3 +73,29 @@ class Loop(object):
     @productivity_model.setter
     def productivity_model(self, value):
         self._productivity_model = value
+
+    def set_models(self, damages_model, carbon_model, prod_frac, params):
+        """
+        Set the models used for damages and oceanic carbon transfer
+        ...
+        Args
+        ----
+        eq: obj, the Dice2007 Loop()
+        damages_model: str, name of damages model from the front-end
+        carbon_model: str, name of carbon model from the front-end
+        prod_frac: float,
+        ...
+        Returns
+        -------
+        None
+        """
+        self.damages_model = getattr(
+            damages, "".join(x.capitalize() for x in damages_model.split('_'))
+        )(prod_frac, params)
+        self.carbon_model = getattr(
+            carbon, "".join(x.capitalize() for x in carbon_model.split('_'))
+        )(params)
+        self.productivity_model = DiceProductivity(params)
+        self.consumption_model = DiceConsumption(params)
+        self.utility_model = DiceUtility(params)
+        self.emissions_model = DiceEmissions(params)
