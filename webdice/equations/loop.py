beb1d99aa952c519659d2e81ff6556f85283e874
Nathan Matteson <matteson@obstructures.org>
2013-06-14 13:29:19 -0500

    Add all submodels and defaults

--- webdice/equations/loop.py
+++ webdice/equations/loop.py
@@ -1,21 +1,20 @@
-import numpy as np
-from damages import *
-from carbon import *
-from emissions import *
-from consumption import *
-from welfare import *
-from productivity import *
+from damages import DiceDamages
+from carbon import DiceCarbon
+from emissions import DiceEmissions
+from consumption import DiceConsumption
+from utility import DiceUtility
+from productivity import DiceProductivity
 
 class Loop(object):
-    """Default equation set."""
+    """Provide getters and setters for model equations"""
     def __init__(self, params, damages_model=DiceDamages,
                  carbon_model=DiceCarbon):
         self._damages_model = damages_model
         self._carbon_model = carbon_model
-        self._emissions_model = EmissionsModel(params)
-        self._consumption_model = ConsumptionModel(params)
-        self._welfare_model = UtilityModel(params)
-        self._productivity_model = ProductivityModel(params)
+        self._emissions_model = DiceEmissions(params)
+        self._consumption_model = DiceConsumption(params)
+        self._utility_model = DiceUtility(params)
+        self._productivity_model = DiceProductivity(params)
 
     @property
     def damages_model(self):
@@ -39,7 +38,7 @@ class Loop(object):
 
     @emissions_model.setter
     def emissions_model(self, value):
-        self._carbon_model = value
+        self._emissions_model = value
 
     @property
     def consumption_model(self):
@@ -50,12 +49,12 @@ class Loop(object):
         self._consumption_model = value
 
     @property
-    def welfare_model(self):
-        return self._welfare_model
+    def utility_model(self):
+        return self._utility_model
 
-    @welfare_model.setter
-    def welfare_model(self, value):
-        self._welfare_model = value
+    @utility_model.setter
+    def utility_model(self, value):
+        self._utility_model = value
 
     @property
     def productivity_model(self):
@@ -64,40 +63,3 @@ class Loop(object):
     @productivity_model.setter
     def productivity_model(self, value):
         self._productivity_model = value
-
-    def welfare(self, utility_discounted, utility_discount):
-        """
-        Objective function
-        ...
-        Returns
-        -------
-        float
-        """
-        return np.sum(utility_discounted)
-
-    def miu(self, emissions_ind, emissions_cap, _e2005, intensity,
-            gross_output):
-        """
-        mu, Emissions reduction rate
-        ...
-        Returns
-        -------
-        float
-        """
-        if emissions_cap == 0:
-            return 1.
-        elif round(emissions_ind, 2) < round((_e2005 * emissions_cap), 2):
-            return 0.
-        else: return 1 - ((_e2005 * emissions_cap) / (intensity * gross_output))
-
-    def tax_rate(self, backstop, miu, abatement_exponent):
-        """
-        Implied tax rate, thousands $USD per ton CO_2
-        ...
-        Returns
-        -------
-        float
-        """
-        return (
-            backstop * miu ** (abatement_exponent - 1) * 1000
-        )
