Derivative fixes, vectorization ...

webdice/dice.py
@@ -43,6 +43,7 @@ class Dice(object):
         self.params._optimize = optimize
         self.eps = self.params._eps
         self.dice_version = 2007
+        self.opt_vars = 30
 
     @property
     def user_params(self):
@@ -135,26 +136,27 @@ class Dice(object):
         -------
         pd.DataFrame : self.data.vars
         """
+        D = self.data.vars
         self.eq.set_models(self.params)
         if self.params._optimize and miu is None:
             self.data.vars.miu = self.get_ipopt_mu()
             self.data.vars.miu[0] = self.params._miu_2005
+        if deriv:
+            self.params.deriv_work.values[:,:] = self.data.vars.values[:]
+            D = self.params.deriv_work.transpose(2,0,1)
         for i in range(self.params._tmax):
-            if not deriv:
-                self.step(i, self.data.vars, miu)
-            else:
-                self.data.deriv = self.data.vars.copy()
-                for j in xrange(i, self.params._tmax):
-                    self.step(j, self.data.deriv, miu, deriv=i==j)
-                self.params._derivative.fprime[i] = (
-                    np.sum(self.data.deriv.utility_discounted) -
-                    np.sum(self.data.vars.utility_discounted)
-                ) / self.eps
+            if deriv:
+                if i < self.opt_vars:
+                    D.miu[i][i] += self.eps
+                miu = D.miu[i]
+            self.step(i, D, miu, deriv=deriv)
         if scc:
             self.get_scc(miu)
         if self.params._optimize and miu is not None:
             if deriv:
-                return self.params._derivative.fprime.transpose()
+                return (
+                    D.utility_discounted.sum(axis=1).values -
+                    self.data.vars.utility_discounted.sum()) / self.eps
             else:
                 return self.welfare
         return self.data.vars
@@ -221,7 +223,7 @@ class Dice(object):
         gl : array, lower bounds of constraints
         gu : array, upper bounds of constraints
         """
-        x0 = np.ones(self.params._tmax)
+        x0 = np.linspace(0, 1, self.opt_vars)
         M = 0
         nnzj = 0
         nnzh = 0
@@ -229,11 +231,15 @@ class Dice(object):
         xu = np.ones(self.params._tmax)
         gl = np.zeros(M)
         gu = np.ones(M) * 4.0
+        def build_x(x):
+            return np.concatenate((
+                x,
+                np.ones(self.params._tmax - self.opt_vars),
+            ))
         def eval_f(x):
-            return self.loop(x, scc=False)
+            return self.loop(build_x(x), scc=False)
         def eval_grad_f(x, *args, **kwargs):
-            # print('0:', x[0], '35:', x[35])
-            return self.loop(x, deriv=True, scc=False)
+            return self.loop(build_x(x), deriv=True, scc=False)
         def eval_g(x):
             return np.zeros(M)
         def eval_jac_g(x, flag):
@@ -243,12 +249,12 @@ class Dice(object):
                 return np.empty(M)
         pyipopt.set_loglevel(0)
         r = pyipopt.create(
-            self.params._tmax, xl, xu, M, gl, gu, nnzj, nnzh, eval_f,
+            self.opt_vars, xl, xu, M, gl, gu, nnzj, nnzh, eval_f,
             eval_grad_f, eval_g, eval_jac_g
         )
         # x, zl, zu, obj, status = r.solve(x0)
         x = r.solve(x0)[0]
-        return x
+        return build_x(x)
 
     def format_output(self):
         """
@@ -283,4 +289,34 @@ class Dice2007(Dice):
 
 
 if __name__ == '__main__':
-    pass
+    # d = Dice2007()
+    # d.loop()
+    # W = d.welfare
+    # d = Dice2007(optimize=True)
+    # d.loop()
+    # print(d.welfare, d.welfare - W)
+    d = Dice2010()
+    d.params.carbon_model = 'beam_carbon'
+    d.loop()
+    W = d.welfare
+    print(W)
+    d = Dice2010(optimize=True)
+    d.params.carbon_model = 'beam_carbon'
+    d.loop()
+    print(d.welfare, d.welfare - W)
+    # print(d.data.vars.miu)
+
+    # import cProfile
+    # d = Dice2007(optimize=True)
+    # cProfile.run('d.loop()')
+
+    # d.params.deriv_work.values[:,:] = d.data.vars.values[:]
+    # a = d.params.deriv_work.transpose(2,0,1)
+    # b = d.data.vars
+    # i = ':,0'
+    # print(a.temp_lower[1])
+    # print(b.temp_lower[1])
+    # a.temp_lower[1] = a.temp_lower[1] + a.temp_atmosphere[1]
+    # print(a.temp_lower)
+    # a.miu[4][4] = 5.
+    # print(a.miu[4])

webdice/equations/carbon.py
@@ -154,7 +154,10 @@ class CarbonModel(object):
             M_AT, M_UP, M_LO at t
         """
         if index == 0:
-            return self.initial_carbon
+            return (data.mass_atmosphere.ix[:][index] / data.mass_atmosphere.ix[:][index] * self.initial_carbon[0],
+                data.mass_upper.ix[:][index] / data.mass_upper.ix[:][index] * self.initial_carbon[1],
+                data.mass_lower.ix[:][index] / data.mass_lower.ix[:][index] * self.initial_carbon[2])
+            # return self.initial_carbon
         i = index - 1
         return (
             self.mass_atmosphere(data.emissions_total[i],
@@ -186,6 +189,17 @@ class BeamCarbon(CarbonModel):
         # self.initial_carbon = [808.9, 772.4, 38620.5]  # from BEAM paper
         # self.initial_carbon = [808.9, 571.5, 38620.5]  # M_UP at H = 10**-8.1
         self.initial_carbon = [808.9, 585, 38620.5]  # Nate's guess
+        self._carbon_matrix_skel = np.array([
+            -.2,
+            .2,
+            0,
+            0, #_b * b_ones
+            0, # (-_b - .05) * b_ones,
+            .05,
+            0,
+            .001,
+            -.001,
+        ]).reshape((3, 3, 1))
 
     def get_h(self, mass_upper):
         """
@@ -203,6 +217,44 @@ class BeamCarbon(CarbonModel):
             6.25e+10 * mass_upper ** 2 - 7.68281e+13 * mass_upper + 2.36815e+16
         ) - 5.0e-7
 
+    def mass_atmosphere(self, emissions_total, mass_atmosphere, mass_upper):
+        """
+        M_AT, Carbon concentration in atmosphere, GtC
+        ...
+        Returns
+        -------
+        float
+        """
+        return (
+            self.carbon_matrix[0][0] * mass_atmosphere + self.carbon_matrix[1][0] *
+            mass_upper + (10 * emissions_total)
+        )
+
+    def mass_upper(self, mass_atmosphere, mass_upper, mass_lower):
+        """
+        M_UP, Carbon concentration in shallow oceans, GtC
+        ...
+        Returns
+        -------
+        float
+        """
+        return (
+            self.carbon_matrix[0][1] * mass_atmosphere + self.carbon_matrix[1][1] *
+            mass_upper + (self.carbon_matrix[2][1] * mass_lower)
+        )
+
+    def mass_lower(self, mass_upper, mass_lower):
+        """
+        M_LO, Carbon concentration in lower oceans, GtC
+        ...
+        Returns
+        -------
+        float
+        """
+        return (
+            self.carbon_matrix[1][2] * mass_upper + self.carbon_matrix[2][2] * mass_lower
+        )
+
     def get_model_values(self, index, data):
         """
         Set BEAM transfer matrix, and return values for M_AT, M_UP, M_LO
@@ -218,7 +270,10 @@ class BeamCarbon(CarbonModel):
             M_AT, M_UP, M_LO at t
         """
         if index == 0:
-            return self.initial_carbon
+            return (data.mass_atmosphere.ix[:][index] / data.mass_atmosphere.ix[:][index] * self.initial_carbon[0],
+                data.mass_upper.ix[:][index] / data.mass_upper.ix[:][index] * self.initial_carbon[1],
+                data.mass_lower.ix[:][index] / data.mass_lower.ix[:][index] * self.initial_carbon[2])
+            # return self.initial_carbon
         i = index - 1
         _ma, _mu, _ml = (
             data.mass_atmosphere[i], data.mass_upper[i], data.mass_lower[i]
@@ -226,15 +281,14 @@ class BeamCarbon(CarbonModel):
         for x in range(self.N):
             _h = self.get_h(_mu)
             _b = (28.944 * _h ** 2) / (_h ** 2 + _h * 1e-6 + 7.53e-16)
-            self.carbon_matrix = np.array([
-                -.2, .2, 0,
-                _b, -_b - .05, .05,
-                0, .001, -.001,
-            ]).reshape(3, 3)
+            self.carbon_matrix = np.tile(self._carbon_matrix_skel, _b.size)
+            self.carbon_matrix[1][0] = _b
+            self.carbon_matrix[1][1] = -_b - .05
             _ma_incr = self.mass_atmosphere(
                 data.emissions_total[i], _ma, _mu) / self.N
             _mu_incr = self.mass_upper(_ma, _mu, _ml) / self.N
             _ml_incr = self.mass_lower(_mu, _ml) / self.N
+
             _ma += _ma_incr
             _mu += _mu_incr
             _ml += _ml_incr
@@ -265,5 +319,5 @@ class Dice2010(CarbonModel):
             self._params._forcing_ghg_2000 + .1 * (
                 self._params._forcing_ghg_2100 - self._params._forcing_ghg_2000
             ) * np.arange(11),
-            self._params._forcing_ghg_2000 * np.ones(49),
+            self._params._forcing_ghg_2000 * (np.ones(49) * .36),
         ))

webdice/equations/emissions.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+import pandas as pd
 
 
 class EmissionsModel(object):
@@ -79,7 +80,8 @@ class EmissionsModel(object):
 
     def get_emissions_values(self, index, data, deriv=False, miu=None,
                              emissions_shock=0):
-        miu = min(self.get_miu(index, data, deriv=deriv, miu=miu), 1.0)
+        # miu = min(self.get_miu(index, data, deriv=deriv, miu=miu), 1.0)
+        miu = self.get_miu(index, data, deriv=deriv, miu=miu)
         emissions_ind = self.emissions_ind(
             data.carbon_intensity[index], miu, data.gross_output[index]
         )
@@ -87,7 +89,7 @@ class EmissionsModel(object):
             emissions_ind, self.emissions_deforest[index]
         ) + emissions_shock
         carbon_emitted = self.carbon_emitted(emissions_total, index, data)
-        if carbon_emitted > self._params.fosslim:
+        if np.max(carbon_emitted) > self._params.fosslim:
             emissions_total = 0.0
             carbon_emitted = self._params.fosslim
         tax_rate = self.tax_rate(miu, data.backstop[index])
@@ -145,30 +147,34 @@ class EmissionsModel(object):
         if self._params._optimize:
             if miu is not None:
                 if deriv:
-                    miu[index] += self.eps
+                    return pd.DataFrame({
+                        'miu': miu,
+                        'ones': np.ones(self._params._tmax),
+                    }).min(axis=1)
                 return miu[index]
         else:
             if index > 0:
                 if data.carbon_emitted[index - 1] > self._params.fosslim:
                     return 1.0
                 if self._params._treaty:
-                    return self.miu(
+                    return min(self.miu(
                         data.emissions_ind[index - 1],
                         self.emissions_cap[index - 1],
                         data.emissions_ind[0],
                         data.carbon_intensity[index], data.gross_output[index]
-                    )
+                    ), 1.0)
                 elif self._params._carbon_tax:
-                    return (
+                    return min(
                         (self.user_tax_rate[index] / (
                             data.backstop[index] * 1000)) ** (
-                            1 / (self._params.abatement_exponent - 1))
+                            1 / (self._params.abatement_exponent - 1)),
+                        1.0
                     )
                 else:
                     return 0
             else:
                 return self._params._miu_2005
-        return data.miu[index]
+        return min(data.miu[index], 1.0)
 
     def miu(self, emissions_ind, emissions_cap, _e2005, intensity,
             gross_output):

webdice/params.py
@@ -174,28 +174,35 @@ class DiceParams(object):
             'deriv': data,
             'scc': data,
         })
+        d_V = {}
+        for i in xrange(self._tmax):
+            d_V[i] = data
+        self.deriv_work = pd.Panel(d_V)
         self._derivative = pd.DataFrame({
             'fprime': np.empty(self._tmax),
         })
-        self._hessian = pd.Series(np.empty(self._tmax))
+        # self._hessian = pd.Series(np.empty(self._tmax))
 
 
 class Dice2010Params(DiceParams):
     def __init__(self):
         super(Dice2010Params, self).__init__(2010)
         self.temp_co2_doubling = 3.2
-        self.damages_exponent = 2.
+        self.damages_exponent = 2.  # TODO: see equations
         self.productivity_decline = .009  # TODO: Add second parameter?
         self.intensity_decline_rate = .00646
         self.popasym = 8700.
         self.elasmu = 1.5
+        ## Population and technology
         self._population_2005 = 6411.
         self._population_growth = .5  # This is called Population adjustment in Dice2010
         self._productivity = .0303220
         self._productivity_growth = .16
         self._output_2005 = 55.34
+        ## Emissions
         self._intensity_2005 = .14452
         self._intensity_growth = .158
+        ## Carbon Cycle
         _b11, _b12, _b13 = .88, .12, 0
         _b21, _b22, _b23 = .04704, .94796, .005
         _b31, _b32, _b33 = 0, .00075, .99925
@@ -204,14 +211,21 @@ class Dice2010Params(DiceParams):
             _b21, _b22, _b23,
             _b31, _b32, _b33,
         ]).reshape(3, 3)
+        # self._mass_atmosphere_2005 = (787 + 829) / 2
+        self._mass_atmosphere_2005 = 787.
         self._mass_upper_2005 = 1600.
         self._mass_lower_2005 = 10100.
+
+        ## Climate model
         self._forcing_ghg_2000 = .83
         self._temp_atmosphere_2000 = .83
         self._temp_atmosphere_2010 = .98
         self.thermal_transfer[0] = .208
         self.thermal_transfer[2] = .310
+
         # self._damages_coefficient = .00204625800317896
+
+        ## Abatement cost
         self._backstop_2005 = 1.26
 
         self._data.vars.intensity_decline[0] = .158