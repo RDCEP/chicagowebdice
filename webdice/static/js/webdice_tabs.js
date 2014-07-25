db61335013451aead60950e60496615fc7fbe27a
Nathan Matteson <matteson@obstructures.org>
2014-07-25 16:10:52 -0500

    Savvier tabs

--- webdice/static/js/webdice_tabs.js
+++ webdice/static/js/webdice_tabs.js
@@ -3,7 +3,8 @@
 
   var parameter_tabs = d3.selectAll('#parameter_tabs li'),
     title_tabs = d3.selectAll('#title_legend li'),
-    parameters = d3.select('#parameters_wrap');
+    parameters = d3.select('#parameters_wrap'),
+    runs = d3.select('#runs_wrap');
 
   parameter_tabs.on('click', function() {
 
@@ -26,19 +27,24 @@
       pane = d3.select('#' + tab),
       parent = d3.select(pane.node().parentNode);
 
-    console.log(pane);
-
-    title_tabs.classed('selected', false);
+    if (t.classed('graph-tab')) {
+      title_tabs.classed('selected', false);
+    } else {
+      d3.selectAll('.option-tab').classed('selected', false);
+    }
     t.classed('selected', true);
 
     if (tab == 'parameters') {
-      parameters.classed('visuallyhidden', false);
+      parameters.classed('visuallyhidden', false).style('z-index', 998);
+      runs.classed('visuallyhidden', true);
     } else if (tab == 'runs') {
       parameters.classed('visuallyhidden', true);
+      runs.classed('visuallyhidden', false).style('z-index', 998);
     } else {
       parameters.classed('visuallyhidden', true);
+      runs.classed('visuallyhidden', true);
       parent.select('.pane.selected').classed('selected', false);
-      pane.classed('selected', true);
+      pane.classed('selected', true).style('z-index', 998);
     }
 
   });
