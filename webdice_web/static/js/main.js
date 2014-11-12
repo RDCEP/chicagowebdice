b0c64b65dd776b72e8f10b565962f5b6c2fb3955
Nathan Matteson <matteson@obstructures.org>
2014-11-12 07:34:51 -0600

    Cleanup

--- webdice_web/static/js/main.js
+++ webdice_web/static/js/main.js
@@ -16,14 +16,14 @@
     initialized = false,
 
     color_list = [
-      d3.rgb(86, 180, 233), // sky blue
-      d3.rgb(230, 159, 0),  // orange
-      d3.rgb(0, 158, 115),  // bluish green
-      d3.rgb(240, 228, 66), // yellow
-      d3.rgb(0, 114, 178),  // blue
-      d3.rgb(213, 94, 0),   // vermilion
+      d3.rgb(86, 180, 233),  // sky blue
+      d3.rgb(230, 159, 0),   // orange
+      d3.rgb(0, 158, 115),   // bluish green
+      d3.rgb(240, 228, 66),  // yellow
+      d3.rgb(0, 114, 178),   // blue
+      d3.rgb(213, 94, 0),    // vermilion
       d3.rgb(204, 121, 167), // reddish purple
-      d3.rgb(50, 50, 50), //black
+      d3.rgb(50, 50, 50)     //black
     ],
     used_colors = [],
     padding = [45, 15, 30, 60],  // padding[3] needs to match $left_pad in _layout.sass
